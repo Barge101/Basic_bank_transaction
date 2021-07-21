40a944ce6aa1da3742a460f2c7eca2da7b5d339d
Barge101 <86842565+Barge101@users.noreply.github.com>
2021-07-21 11:09:13 +0530

    Update customer.php

--- bank_project/customer.php
+++ bank_project/customer.php
@@ -87,7 +87,7 @@
             <?php
              $servername="localhost";
              $username="root";
-             $password="Shubham@101";
+             $password="";
              $database_name="bank_data";
 
              $conn=mysqli_connect($servername,$username,$password,$database_name);
